8f396661884889596394ca9600012633d5a9ad37
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-20 14:24:05 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,50 @@
+// String
+// let score = "22anb"
+
+// console.log(typeof score) 
+// // console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+
+
+// // null value 
+// let mark = null
+
+// console.log(typeof mark)
+
+// let value = Number(mark)
+// console.log(typeof value)
+// console.log(value);
+
+
+
+// // null value 
+// let val = undefined
+
+// console.log(typeof val)
+
+// let con = Number(val)
+// console.log(typeof val)
+// console.log(val);
+
+
+// "33" => 33
+// "33abc" => Nan--> Not a Number 
+// true => 1
+// false => 0
+// "" => false
+// "Umair" => true
+
+
+// let isLoggedIn = "Umair"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 22
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
