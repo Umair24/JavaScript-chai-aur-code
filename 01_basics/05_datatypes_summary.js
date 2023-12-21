18aa0fa0d2ad03384fbee7c1e7e90671868b6441
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-21 14:30:51 +0000

    Data Type Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,14 @@
+// Primitive Datatype
+// 7 types: String, Boolean, number, null, undefined, symbol, BigInt
+
+const score = 200
+const scoreValue = 150.5
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail = undefined
+const id = Symbol('253')
+const anotherId = Symbol('253')
+
+// Non- primitive/ Reference
+// Array, Object, Function 
