b21c08032bf073640af670249a31aca971567968
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-21 06:53:05 +0000

    Conversion of datatypes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -44,7 +44,44 @@
 // console.log(booleanIsLoggedIn);
 
 
-let someNumber = 22
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let someNumber = 22
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+
+                // Operations
+// let value = 5
+// let negValue = -value
+// console.log(value);
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/5);
+// console.log(2%5);
+
+
+// let str1 = "Hello"
+// let str2 = " Umair"
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + "2" + 2)
+// console.log("1" + 2 + 2)
+// console.log("1" * 2 * 2)
+// console.log(true)
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+++gameCounter;
+console.log(gameCounter);
