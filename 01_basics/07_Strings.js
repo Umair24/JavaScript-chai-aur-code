47f6ca32cfdcfffd6441e1bff2441188a3f36e1b
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-22 15:47:44 +0000

    Advance Strings Methods

---
+++ 01_basics/07_Strings.js
@@ -0,0 +1,26 @@
+// let name = "Umair Ansari"
+// let myage = "22"
+
+// // console.log("My name is " + Name +"and my age is "+ age );  ---> This is an old method to concatenate the two strings
+
+// console.log(`Hello my name is ${name} and my age is ${myage}.`);   // This an new method to concatenate the two strings
+
+
+// alternate method to declare a String
+// let gameName = new String ("Ansari Md. Umair")
+// console.log(gameName);
+
+// let gameName = new String ("Ansari Md. Umair")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);  //---> to check all prototypes
+// console.log(gameName.length);  //---> to check length of the string
+// console.log(gameName.toUpperCase());  //---> it is used for print string in upper case
+// console.log(gameName.charAt(5));  //---> to check the position of a character
+// console.log(gameName.indexOf('n'));  //---> to check the position of a character reverse to charAt function
+// console.log(gameName.substring(0, 5));  //---> ot is use for slicing a string
+// console.log(gameName.slice(-8, 2));  //---> it is used for slicing a string and we can use negative values in slice method
+
+
+// let newString = "  Umair  "
+// console.log(newString);
+// console.log(newString.trim());  //--> it is used for trim the spaces from a string
