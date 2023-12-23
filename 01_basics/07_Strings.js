ece66c40c9340cf5cf382ec1dcd439ceabbbc3bd
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-23 04:11:47 +0000

    Advance Strings Methods

--- 01_basics/07_Strings.js
+++ 01_basics/07_Strings.js
@@ -6,10 +6,10 @@
 // console.log(`Hello my name is ${name} and my age is ${myage}.`);   // This an new method to concatenate the two strings
 
 
+
 // alternate method to declare a String
 // let gameName = new String ("Ansari Md. Umair")
 // console.log(gameName);
-
 // let gameName = new String ("Ansari Md. Umair")
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);  //---> to check all prototypes
@@ -19,8 +19,15 @@
 // console.log(gameName.indexOf('n'));  //---> to check the position of a character reverse to charAt function
 // console.log(gameName.substring(0, 5));  //---> ot is use for slicing a string
 // console.log(gameName.slice(-8, 2));  //---> it is used for slicing a string and we can use negative values in slice method
+// console.log(gameName.split(" "));  //--> split method is used for spliting a string
 
 
 // let newString = "  Umair  "
 // console.log(newString);
 // console.log(newString.trim());  //--> it is used for trim the spaces from a string
+
+
+// let ulr = "https://www.google.com/gmail/"
+// console.log(ulr.replace('com', 'gov'));  //--> this replace method is use for replacing a string in a variable
+// console.log(ulr.includes('google'));
+// console.log(ulr.includes('gov'));
