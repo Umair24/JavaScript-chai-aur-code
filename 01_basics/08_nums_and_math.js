17c4b9c48c9c510743b20d61dddb13e32a02f848
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-23 05:56:38 +0000

    Numbers and Maths

---
+++ 01_basics/08_nums_and_math.js
@@ -0,0 +1,22 @@
+// let score = 555  //---> first method to print a number
+// console.log(score);  
+
+
+// let balance = new Number(300)  //---> second method to print a number
+// console.log(balance);
+// console.log(balance.toString().length);  //--> tostring is used to convert a number into a string
+// console.log(balance.toFixed(5));
+
+
+// let newNumber = 125.555
+// console.log(newNumber.toPrecision(3));  //--> toPrecision() method used to precise the value
+
+
+// let hund = 100000000
+// console.log(hund.toLocaleString());  //---> it is used to format a number
+// console.log(hund.toLocaleString('en-IN'));  //--> we can also format a number in our local format
+
+
+
+
+//---------------------MATHS------------------
