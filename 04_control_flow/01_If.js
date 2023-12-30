6c3c77e25f1ee9d368a20ccc725a508b1cefe62f
Umair Ansari <79690766+Umair24@users.noreply.github.com>
2023-12-30 05:18:15 +0000

    truthy values

--- 04_control_flow/01_If.js
+++ 04_control_flow/01_If.js
@@ -36,14 +36,29 @@
 
 
 
-let userLoggedIn = true
-let debitCard = true
-let loggedInFromGoogle = false
-let loggedInFromEmail = true
+// let userLoggedIn = true
+// let debitCard = true
+// let loggedInFromGoogle = false
+// let loggedInFromEmail = true
 
-if(userLoggedIn && debitCard){
-    console.log("Allow to buy courses");
-}
-if(loggedInFromGoogle || loggedInFromEmail){
-    console.log("user Logged in!");
-}
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy courses");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("user Logged in!");
+// }
+
+
+// Nullish coaleseing operator (??): null undefined
+//  let val1;
+// //  val1 = 5 ?? 10
+// //  val1 = null ?? 10
+// // val1 = undefined ?? 15
+// val1 = null ?? 10 ??15
+//  console.log(val1);
+
+
+//  Terniary operator
+// condition ? true : false
+let iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
